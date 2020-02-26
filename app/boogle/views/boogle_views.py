e94cb49518c5f4a362d4b24d4b546d12ec0f8caa
iqDF <daniel.thekurniadi@gmail.com>
2020-02-26 20:16:49 +0800

    [App/Boogle] http router - provide http status code - validator decorator for request input

--- app/boogle/views/boogle_views.py
+++ app/boogle/views/boogle_views.py
@@ -6,23 +6,22 @@ from flask import Blueprint, request, jsonify
 
 from app.boogle.service import import boogle_manager
 from app.boogle.views.boogle_validators import (
-    update_game_validator,
-    create_game_validator
+    validate_update_game_request,
+    validate_create_game_request
 )
-from app.bridge.error import get_msg_error, get_http_status_code
+from app.bridge.error import get_msg_error
 
 mod_boogle = Blueprint('boogle', __name__)
 
 
 @mod_boogle.route('/games/<game_id>', methods=['GET'])
-@create_game_validator
 def api_get_boogle(game_id):
     error_code, boogle_game = boogle_manager.get_boogle_by_game_id(game_id)
 
     return jsonify({
         **get_msg_error(error_code),
         **boogle_game
-    })
+    }), error_code.http_code
 
 
 @mod_boogle.route('/games/<game_id>', methods=['PUT'])
@@ -34,10 +33,11 @@ def api_put_boogle(game_id):
     return jsonify({
         **get_msg_error(error_code),
         **boogle_game
-    })
+    }), error_code.http_code
 
 
 @mod_boogle.route('/games/', methods=['POST'])
+@validate_create_game_request
 def api_post_boogle():
     payload = dict(request.json)
     error_code, boogle_game = boogle_manager.create_boogle_game(**payload)
@@ -45,4 +45,4 @@ def api_post_boogle():
     return jsonify({
         **get_msg_error(error_code),
         **boogle_game
-    })
+    }), error_code.http_code
