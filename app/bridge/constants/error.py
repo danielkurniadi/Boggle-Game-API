e21f94aa875287754cfce3828c3b557665689056
iqDF <daniel.thekurniadi@gmail.com>
2020-02-26 20:31:12 +0800

    [App/Bridge] Error class add http_code attr

--- app/bridge/constants/error.py
+++ app/bridge/constants/error.py
@@ -1,10 +1,12 @@
 class ServerOk:
     error_code = 0
+    http_code = 200
 
 
 class Error:
     message = ''
     error_code = 1
+    http_code = 400
 
     def __init__(self, message):
         self.message = message
@@ -12,11 +14,14 @@ class Error:
 
 class DatabaseError(Error):
     error_code = 2
+    http_code = 404
 
 
 class OperationNotSupported(Error):
     error_code = 3
+    http_code = 400
 
 
 class InvalidRequest(Error):
-    error_code = 99
+    error_code = 4
+    http_code = 400
