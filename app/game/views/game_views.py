6d10266587a07a6be95cd7dfb1d4873be1121592
iqDF <daniel.thekurniadi@gmail.com>
2020-02-27 11:47:35 +0800

    [App/Game] refactor application - entities names with domain driven pattern

--- app/game/views/game_views.py
+++ app/game/views/game_views.py
@@ -4,20 +4,20 @@ This module is for boogle API related
 
 from flask import Blueprint, request, jsonify
 
-from app.boogle.service import boogle_service
-from app.boogle.views.boogle_validators import (
+from app.game.libs import game_manager
+from app.game.views.game_validators import (
     validate_update_game_request,
     validate_create_game_request
 )
 
-mod_boogle = Blueprint('boogle', __name__)
+mod_game = Blueprint('boogle', __name__)
 
 
-@mod_boogle.route('/games/', methods=['POST'])
+@mod_game.route('/games/', methods=['POST'])
 @validate_create_game_request
-def api_post_boogle():
+def api_post_game():
     payload = dict(request.json)
-    error_code, boogle_game = boogle_service.create_boogle_game(**payload)
+    error_code, boogle_game = game_manager.create_boogle_game(**payload)
 
     return jsonify({
         **error_code.to_dict(),
@@ -25,9 +25,9 @@ def api_post_boogle():
     }), error_code.http_code
 
 
-@mod_boogle.route('/games/<game_id>', methods=['GET'])
-def api_get_boogle(game_id):
-    error_code, boogle_game = boogle_service.get_boogle_game(game_id)
+@mod_game.route('/games/<game_id>', methods=['GET'])
+def api_get_game(game_id):
+    error_code, boogle_game = game_manager.get_boogle_game(game_id)
 
     return jsonify({
         **error_code.to_dict(),
@@ -35,11 +35,11 @@ def api_get_boogle(game_id):
     }), error_code.http_code
 
 
-@mod_boogle.route('/games/<game_id>', methods=['PUT'])
+@mod_game.route('/games/<game_id>', methods=['PUT'])
 @validate_update_game_request
-def api_put_boogle(game_id):
+def api_put_game(game_id):
     payload = dict(request.json)
-    error_code, boogle_game = boogle_service.update_boogle_game(game_id, **payload)
+    error_code, boogle_game = game_manager.update_boogle_game(game_id, **payload)
 
     return jsonify({
         **error_code.to_dict(),
