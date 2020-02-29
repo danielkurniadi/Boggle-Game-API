1340dbb48672d65bb347609bcac8ef9be9a118f5
iqDF <daniel.thekurniadi@gmail.com>
2020-02-29 18:41:38 +0800

    [App/Game] refactor db_exception decorator

--- app/bridge/decorators/db_exception.py
+++ app/bridge/decorators/db_exception.py
@@ -1,13 +1,25 @@
 from functools import wraps
-from app import flask_app
-from app.bridge.error.error_code import ServerOk, DatabaseError
+from mongoengine import errors as mongod_error
 
+from app import flask_app
+from app.bridge.error.error_code import (
+    ServerOk, ResourceNotFound,
+    DatabaseError, OperationNotSupported,
+)
 
 def db_exception(f):
     @wraps(f)
-    def wrapped(*args, **kwargs):
+    def wrapped(*args, full=False, **kwargs):
         try:
-            return f(*args, **kwargs)
+            result = f(*args, **kwargs)
+            if result is None:
+                return ResourceNotFound('Resource Not Found')
+            return result.to_json(full=full)
+
+        except (mongod_error.NotUniqueError, mongod_error.ValidationError) as e:
+            flask_app.logger.error('function: %s| error: %s', f, e)
+            return OperationNotSupported(str(e))
+
         except Exception as e:
             flask_app.logger.error('function: %s| error: %s', f, e)
             return DatabaseError(str(e))
