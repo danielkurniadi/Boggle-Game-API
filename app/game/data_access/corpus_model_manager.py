ec705398d0c50496484d349d02bb8d21ca70d4e3
iqDF <daniel.thekurniadi@gmail.com>
2020-02-29 18:18:36 +0800

    [App/Game] refactpr corpus model manager

--- app/game/data_access/corpus_model_manager.py
+++ app/game/data_access/corpus_model_manager.py
@@ -1,20 +1,21 @@
 import os
 
-from app import flask_app
 from app.bridge.error.error_code import OperationNotSupported
-from app.bridge.managers.file_manager import load_words_from_corpus_file
+from app.bridge.managers.corpus_file_manager import load_words_from_corpus_file
 from app.game.models.corpus_model import Corpus, PrefixTrieNode
 
 
-CORPUS_PATH = flask_app.config['CORPUS_PATH']
-
-
 # --------------------------
-# PREFIX TRIE
+# MODEL MANAGER
 # --------------------------
 
-def get_or_create_corpus(corpus_path=CORPUS_PATH):
-    corpus = Corpus.objects(filepath=corpus_path).first()
+def get_corpus_by_name(name):
+    corpus = Corpus.objects(name=name).first()
+    return corpus
+
+
+def get_or_create_corpus(corpus_name, corpus_path):
+    corpus = Corpus.objects(name=corpus_name).first()
 
     if corpus is None:
         if not os.path.isfile(corpus_path):
@@ -24,17 +25,23 @@ def get_or_create_corpus(corpus_path=CORPUS_PATH):
                 "nor it is found in specified path: %s" % corpus_path
             )
         corpus_words = load_words_from_corpus_file(corpus_path)
-        prefix_trie = build_prefix_trie(words)
+        prefix_trie = build_prefix_trie(corpus_words)
 
-        corpus = Corpus(filepath = corpus_path, name=corpus_path.split('.')[0])
+        corpus = Corpus(filepath=corpus_path, name=corpus_name,
+                        prefix_trie=prefix_trie)
         corpus.save()
 
-    return corpus.to_json()
+    return corpus
 
+
+# --------------------------
+# PREFIX TRIE
+# --------------------------
 
 def build_prefix_trie(words):
     prefix_trie = PrefixTrieNode()
     for word in words:
+        word = word.upper()
         prefix_trie.insert(word)
 
     return prefix_trie
