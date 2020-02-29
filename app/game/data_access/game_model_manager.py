a0e1d50fdd40b64b64f6326d5c42e16f03ae3957
iqDF <daniel.thekurniadi@gmail.com>
2020-02-29 21:10:40 +0800

    [App/Game] defend against invalid game id

--- app/game/data_access/game_model_manager.py
+++ app/game/data_access/game_model_manager.py
@@ -1,10 +1,11 @@
+import bson
 from app import db
 from app.game.models.game_model import Game
 from app.bridge.decorators.db_exception import (
     db_exception
 )
 from app.bridge.error.error_code import (
-    OperationNotSupported
+    OperationNotSupported, ResourceNotFound
 )
 
 TOKEN_LENGTH = 32
@@ -16,12 +17,30 @@ TOKEN_LENGTH = 32
 
 @db_exception
 def get_game_by_id(game_id):
-    return Game.objects(id=game_id).first()
+    if not bson.ObjectId.is_valid(game_id):
+        return ResourceNotFound('Invalid game_id.')
+
+    game = Game.objects(id=game_id).first()
+
+    if isinstance(game, Game) and game.check_expired():
+        game.delete()
+        return OperationNotSupported("Outdated Game. Time's Up.")
+
+    return game
 
 
 @db_exception
 def get_game_by_token(token):
-    return Game.objects(token=token).first()
+    if not bson.ObjectId.is_valid(game_id):
+        return ResourceNotFound('Invalid game_id.')
+
+    game = Game.objects(token=token).first()
+
+    if isinstance(game, Game) and game.check_expired():
+        game.delete()
+        return OperationNotSupported("Outdated Game. Time's Up.")
+
+    return game
 
 
 @db_exception
@@ -34,7 +53,10 @@ def create_game(duration, token, board_id):
 
 @db_exception
 def update_game(game_id, token, word):
-    word = word.upper()
+    if not bson.ObjectId.is_valid(game_id):
+        return ResourceNotFound('Invalid game_id.')
+
+    upper_word = word.upper()
     game = Game.objects(id=game_id, token=token).first()
 
     if game is None:
@@ -45,14 +67,12 @@ def update_game(game_id, token, word):
     if game.check_expired():
         return OperationNotSupported("Outdated Game. Time's Up.")
 
-    if not board.check_answer(word):
+    if not board.check_answer(upper_word):
         return OperationNotSupported("Wrong answer: %s" % word)
     
-    if not game.check_repeat(word):
-        # add points for correct answer
-        # which is not a repeat
-        game.found_words.append(word)
-        game.increment_points()
+    if not game.check_repeat(upper_word):
+        game.found_words.append(upper_word)
+        game.increment_points(len(upper_word))
         game.save()
 
     return game
